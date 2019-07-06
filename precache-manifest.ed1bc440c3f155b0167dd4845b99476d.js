84323e3f5ea7327aeeba49ec1eba664a9d48d514
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-06 00:49:34 +0000

    Deploy dotku/pen to github.com/dotku/pen.git:gh-pages

---
+++ precache-manifest.ed1bc440c3f155b0167dd4845b99476d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02531c4077d4c56dbd196666bbadbcdb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "181236420456ee14afe8",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d8065c423c6e0f8eb378",
+    "url": "/static/js/2.203db3c7.chunk.js"
+  },
+  {
+    "revision": "181236420456ee14afe8",
+    "url": "/static/js/main.b12a9e27.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
