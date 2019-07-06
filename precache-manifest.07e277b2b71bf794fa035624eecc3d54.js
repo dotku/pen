42885cba5496f27eec867f7cfd15c132f56a31c5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-06 01:04:56 +0000

    Deploy dotku/pen to github.com/dotku/pen.git:gh-pages

---
+++ precache-manifest.07e277b2b71bf794fa035624eecc3d54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "710ecd33574b019d505d0fe2954100a2",
+    "url": "/pen/index.html"
+  },
+  {
+    "revision": "f5842b5a98c22b98df00",
+    "url": "/pen/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d8065c423c6e0f8eb378",
+    "url": "/pen/static/js/2.203db3c7.chunk.js"
+  },
+  {
+    "revision": "f5842b5a98c22b98df00",
+    "url": "/pen/static/js/main.22f1a794.chunk.js"
+  },
+  {
+    "revision": "b376e7d45abc923a8e6a",
+    "url": "/pen/static/js/runtime~main.a987f65f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ed1bc440c3f155b0167dd4845b99476d.js"
+  "/pen/precache-manifest.07e277b2b71bf794fa035624eecc3d54.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pen/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
